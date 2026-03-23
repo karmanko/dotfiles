a9b3c9bfd6ad6971a391f0afc0b59c7887600d50
karmanko <karmanov.ivan09@yandex.ru>
2026-03-23 15:55:25 +0300

    my CachyOS setup

---
+++ config/fish/conf.d/fish_frozen_theme.fish
@@ -0,0 +1,47 @@
+# This file was created by fish when upgrading to version 4.3, to migrate
+# theme variables from universal to global scope.
+# Don't edit this file, as it will be written by the web-config tool (`fish_config`).
+# To customize your theme, delete this file and see
+#     help interactive#syntax-highlighting
+# or
+#     man fish-interactive | less +/^SYNTAX.HIGHLIGHTING
+# for appropriate commands to add to ~/.config/fish/config.fish instead.
+# See also the release notes for fish 4.3.0 (run `help relnotes`).
+
+set --global fish_color_autosuggestion bfaab2
+set --global fish_color_cancel --reverse
+set --global fish_color_command eba4b0
+set --global fish_color_comment c8b0b8
+set --global fish_color_cwd eba4b0
+set --global fish_color_cwd_root ff8fa3
+set --global fish_color_end c8b0b8
+set --global fish_color_error ff8fa3
+set --global fish_color_escape d8b6c0
+set --global fish_color_history_current --bold
+set --global fish_color_host normal
+set --global fish_color_host_remote d0b8bf
+set --global fish_color_keyword d2a6ad
+set --global fish_color_normal f0d8dc
+set --global fish_color_operator eba4b0
+set --global fish_color_option d8b6c0
+set --global fish_color_param e0d4d8
+set --global fish_color_quote e0d4d8
+set --global fish_color_redirection c8b0b8
+set --global fish_color_search_match bryellow --bold --background=white
+set --global fish_color_selection white --bold --background=brblack
+set --global fish_color_status ff8fa3
+set --global fish_color_user eba4b0
+set --global fish_color_valid_path --underline=single
+set --global fish_pager_color_background
+set --global fish_pager_color_completion green
+set --global fish_pager_color_description B3A06D
+set --global fish_pager_color_prefix cyan --underline=single
+set --global fish_pager_color_progress brwhite --bold --background=cyan
+set --global fish_pager_color_secondary_background
+set --global fish_pager_color_secondary_completion
+set --global fish_pager_color_secondary_description
+set --global fish_pager_color_secondary_prefix
+set --global fish_pager_color_selected_background --background=white
+set --global fish_pager_color_selected_completion
+set --global fish_pager_color_selected_description
+set --global fish_pager_color_selected_prefix
